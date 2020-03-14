9f78df950c6c2fbe62d5a34926a4d271fa651333
SafinWasi <safinwasi_11@outlook.com>
2020-03-14 15:18:13 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+sdk = require("matrix-bot-sdk");
+MatrixClient = sdk.MatrixClient;
+SimpleFsStorageProvider = sdk.SimpleFsStorageProvider;
+
+class TestingBot {
+	constructor(homeserverUrl, accessToken) {
+		this.homeserverUrl = homeserverUrl;
+		this.accessToken = accessToken;
+		this.AutojoinRoomsMixin = sdk.AutojoinRoomsMixin;
+		this.storage = new SimpleFsStorageProvider("bot.json");
+		this.client = new MatrixClient(homeserverUrl, accessToken, this.storage);
+	}
+
+	init() {
+		const client = this.client;
+		client.start().then(() => console.log("Client started!"));	
+		client.on("room.message", (roomId, event) => {
+			this.messageHandler(roomId, event);
+		});
+	}
+
+	messageHandler(roomId, event) {
+			const client = this.client;
+    		if (! event["content"]){
+				return;
+			}
+    		const sender = event.sender;
+    		const body = event.content.body;
+    		console.log(`${roomId}: ${sender} says '${body}`);
+    		if (body.startsWith("!echo")) {
+        		const replyText = body.substring("!echo".length).trim();
+        		client.sendMessage(roomId, {
+            		"msgtype": "m.notice",
+            		"body": replyText,
+        		});
+    		} else if (body.startsWith("!die")) {
+				console.log("Goodbye!");
+				process.exit(0);
+			}
+	}
+}
+
+function read() {
+	fs.readFile('./config.json', 'utf8', (err, jsonString) => {
+    	if (err) {
+        	console.log("File read failed:", err)
+        	process.exit(0);
+    	}
+		try {
+			const config = JSON.parse(jsonString);
+			setup(config);
+		}
+		catch(err){
+			console.log("Error parsing JSON data: ", err);
+		} 
+	})
+}
+
+function setup(config) {
+	const homeserverUrl = config.homeserverUrl;
+	const accessToken = config.accessToken;
+	const bot = new TestingBot(homeserverUrl, accessToken);
+	bot.init();
+}
+
+read();
